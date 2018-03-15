1830495a05da3781e49b8fef4e25ee6d485acd31
Matt <bassmattf@gmail.com>
2018-03-15 17:21:24 -0500

    Buggy but working error handling on input.

--- WebScraper.py
+++ WebScraper.py
@@ -31,52 +31,82 @@ def infoGrabber( linkList ):
         
     return;
 
-pageQuote = "https://www.uah.edu/cgi-bin/schedule.pl?"
 
-mainLinks = []
-springLinks = []
-summerLinks = []
-fallLinks = []
+def MenuPrint():
+    print("What semester would you like to view classes for?")
+    semesterList = ["Spring", "Summer", "Fall"]
 
-linkGrabber( pageQuote , mainLinks)
+    idx = 1
+    for semester in semesterList:
+        print ('{}.\t{}'.format(idx,semester))
+        idx = idx + 1
 
-for links in mainLinks:
-    
-    if links.find('sprg') != -1:
-        linkGrabber(links, springLinks)
+def getSemester():
+    userAns = input(">>> ")
+    validChoiceChosen = 0
+    while validChoiceChosen==0:
+        try:
+            userAns = int(userAns)
+        except ValueError:
+            print("Not a Number")
+            MenuPrint()
+            userAns = input(">>> ")
+        validChoice = [1,2,3]
+        if userAns in validChoice:
+            validChoiceChosen = 1
+            break
+        else:
+            print("Invalid Number.")
+            MenuPrint()
+            userAns = input(">>> ")
+    return userAns
+
+def main():
+    pageQuote = "https://www.uah.edu/cgi-bin/schedule.pl?"
+
+    mainLinks = []
+    springLinks = []
+    summerLinks = []
+    fallLinks = []
+
+    linkGrabber( pageQuote , mainLinks)
+
+    for links in mainLinks:
         
-    if links.find("sum") != -1:
-        linkGrabber(links, summerLinks)
+        if links.find('sprg') != -1:
+            linkGrabber(links, springLinks)
+            
+        if links.find("sum") != -1:
+            linkGrabber(links, summerLinks)
 
-    if links.find("fall") != -1:
-        linkGrabber(links, fallLinks)
+        if links.find("fall") != -1:
+            linkGrabber(links, fallLinks)
 
-linkFormatter(springLinks)
-linkFormatter(summerLinks)
-linkFormatter(fallLinks)
-
-
-print("What semester would you like to view classes for?")
-
-
-semesterList = ["Spring", "Summer", "Fall"]
-
-idx = 1
-for semester in semesterList:
-    print ('{}.\t{}'.format(idx,semester))
-    idx = idx + 1
-userAns = int(input(">>> "))
-
-
-if userAns == 1:
-    infoGrabber(springLinks)
-elif userAns == 2:
-    infoGrabber(summerLinks)
-elif userAns == 3:
-    infoGrabber(fallLinks)
-else:
-    print("Invalid input. Ending program.\n")
-    
+    linkFormatter(springLinks)
+    linkFormatter(summerLinks)
+    linkFormatter(fallLinks)
 
 
 
+    #print out the menu
+    MenuPrint()
+
+
+    #Cue for input from user
+    userAns = getSemester()
+
+
+    #TODO error checking on input
+    if userAns == 1:
+        infoGrabber(springLinks)
+    elif userAns == 2:
+        infoGrabber(summerLinks)
+    elif userAns == 3:
+        infoGrabber(fallLinks)
+    # else:
+    #     print("Invalid input. Ending program.\n")
+        
+
+
+if __name__=="__main__":
+    main()
