e483f9a47f79a46ac41e3a07cab7b7362cc291d8
Duke02 <trystanmay@gmail.com>
2018-03-18 00:03:17 -0500

    Updated comments

--- parseAllCourseInfo.py
+++ parseAllCourseInfo.py
@@ -1,5 +1,7 @@
 from parseInformation import parseInformation
 
+# This function takes all of the course listings and
+# parses each one and puts it into a list of Course objects
 def parseAllCourseInfo(courseList):
     out = []
     for line in courseList:
